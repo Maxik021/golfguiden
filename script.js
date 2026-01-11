911bb3b0f6519de594643d0767b3bbf5d805a649
Maxik021 <maxrolfkarlsson@outlook.com>
2026-01-11 14:47:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,25 +19,32 @@ const places = [
   }
 ];
 
-const list = document.getElementById("list");
+// Hämta rätt "lådor" från HTML
+const outdoorList = document.getElementById("outdoor-list");
+const indoorList = document.getElementById("indoor-list");
 
+// Loopa igenom alla platser
 places.forEach(place => {
-  const div = document.createElement("div");
-  div.className = "card";
+  const card = document.createElement("div");
+  card.className = "card";
 
-  let info = "";
-
-  if (place.type === "utegolf") {
-    info = `Greenfee: ${place.greenfee} kr`;
-  } else {
-    info = `Pris per timme: ${place.pricePerHour} kr`;
-  }
-
-  div.innerHTML = `
-    <h3>${place.name}</h3>
-    <p>${info}</p>
+  // Gemensamt innehåll
+  let content = `
+    <h4>${place.name}</h4>
     <p>Studentrabatt: ${place.student ? "Ja" : "Nej"}</p>
   `;
 
-  list.appendChild(div);
+  // Utegolf
+  if (place.type === "utegolf") {
+    content += `<p>Greenfee: ${place.greenfee} kr</p>`;
+    card.innerHTML = content;
+    outdoorList.appendChild(card);
+  }
+
+  // Inomhusgolf
+  if (place.type === "inomhusgolf") {
+    content += `<p>Pris per timme: ${place.pricePerHour} kr</p>`;
+    card.innerHTML = content;
+    indoorList.appendChild(card);
+  }
 });
